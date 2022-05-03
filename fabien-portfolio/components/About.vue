8c97e7d47b704c588a22795a0f749e1a73d0b436
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-05-03 20:46:30 +0200

    Text incorporer (a revoir) Responsive image bof

--- fabien-portfolio/components/About.vue
+++ fabien-portfolio/components/About.vue
@@ -8,17 +8,20 @@
             <br/>Web Developer
           </span>
         <div class="hidden md:block">
-          <p class="max-w-xl pt-8 font-semibold text-indigo-300 sm:text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur dolore eum exercitationem illo ipsam iste numquam quibusdam sed sit tempora ut vero! Consequuntur cum qui quibusdam sapiente voluptas voluptates!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, autem et facere fuga fugit illo, ipsa iure libero minima, nobis nostrum quod quos recusandae suscipit voluptatem? Beatae iste quibusdam similique.
-          </p>
+          <p class="max-w-xl pt-8 font-semibold text-indigo-300 sm:text-2xl">
+            Passionné par la digitalisation et la création, je me suis réorienté vers le développement web.  En effet, j’ai déjà eu l’occasion de découvrir le monde du travail et de me former aux exigences de ce dernier.</p>
+          <p class="max-w-xl pt-8 font-semibold text-indigo-300 sm:text-2xl">
+           Polyvalent et flexible, je suis toujours désireux d'en savoir plus sur les technologies émergentes.</p>
+          <p class="max-w-xl pt-8 font-semibold text-indigo-300 sm:text-2xl">
+            J’espère que cette introduction vous donnera envie de me contacter et d’en savoir plus sur ce que je peux apporter à votre entreprise et à vos équipes.</p>
+
           <div class="pt-12">
             <button class="mr-4 btn btn-primary" v-scroll-to="'#projects'">Mes projets</button>
             <button class="btn btn-secondary" v-scroll-to="'#contact'">Contact</button>
           </div>
         </div>
       </div>
-      <div class="flex items-center hidden lg:block">
+      <div class="flex items-center hidden md:block">
         <img
           class="h-98 bg-gray-900 rounded-full shadow-xl"
           src="../assets/img/unnamed.jpg"
@@ -26,15 +29,18 @@
         />
       </div>
     </div>
-<!--    <div class="xl:hidden">-->
-<!--      <p class="pt-8 text-lg font-semibold text-indigo-300 sm:text-xl">-->
-<!--        lalalalalLorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus dolores esse eum illo molestiae nostrum quidem voluptatibus. Fuga illo ipsum laboriosam magni maiores nulla obcaecati quas quo repellendus totam.-->
-<!--      </p>-->
-<!--      <div class="pt-8">-->
-<!--        <button class="mr-4 btn btn-primary" v-scroll-to="'#projects'">My Work</button>-->
-<!--        <button class="mt-4 btn btn-secondary" v-scroll-to="'#contact'">Contact</button>-->
-<!--      </div>-->
-<!--    </div>-->
+    <div class="md:hidden">
+      <p class="pt-8 text-lg font-semibold text-indigo-300 sm:text-xl">
+        Passionné par la digitalisation et la création, je me suis réorienté vers le développement web.  En effet, j’ai déjà eu l’occasion de découvrir le monde du travail et de me former aux exigences de ce dernier.
+        Polyvalent et flexible, je suis toujours désireux d'en savoir plus sur les technologies émergentes.
+        J’espère que cette introduction vous donnera envie de me contacter et d’en savoir plus sur ce que je peux apporter à votre entreprise et à vos équipes.</p>
+
+      </p>
+      <div class="pt-8">
+        <button class="mr-4 btn btn-primary" v-scroll-to="'#projects'">Mes projets</button>
+        <button class="mt-4 btn btn-secondary" v-scroll-to="'#contact'">Contact</button>
+      </div>
+    </div>
   </section>
   </template>
 
