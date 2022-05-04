a7dd8aead59a07c817660405255fe7e23403aedc
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-05-04 14:15:31 +0200

    Expérience done à revoir la taille du text ansi que les descriptif

--- fabien-portfolio/components/Experience.vue
+++ fabien-portfolio/components/Experience.vue
@@ -11,7 +11,7 @@
                   Projet Web EPITECH
               </span>
           <span class="block text-lg text-white uppercase">Novembre 2021 - En cours</span>
-          <div class="pt-4 text-lg font-semibold text-indigo-300 sm:text-xl">
+          <div class="pt-4 font-semibold text-indigo-300 text-xl sm:text-xl">
             <p>
               Plus en détail, je suis formé sur des projets pratiques de façon hebdomadaire, avec un temps imparti à la réalisation de chaque projet et la possibilité de travailler sur plusieurs projets en parallèle, l’objectif étant d’être en capacité d’être polyvalent et flexible.
             </p>
@@ -38,7 +38,23 @@
               Gestion globale de l’exploitation, établir le planning des employés, recruter du personnel et le former aux procédures de l'établissement  ,maintien d’une qualité d’activité constante et productive.
             </p>
           </div>
-        </div>
+        </div><div class="w-full my-10 border-b border-indigo-300 opacity-25"></div>
+          <div>
+              <span
+                class="block text-2xl font-semibold text-white uppercase"
+              >Divers</span>
+            <span class="block text-lg text-white uppercase">Paimpol</span>
+            <span class="block text-lg text-white uppercase">2012 - 2014 2 ans</span>
+            <div class="pt-4 text-lg text-indigo-300 font-semibold sm:text-xl">
+              <p>
+                En effet, j’ai déjà eu l’occasion de découvrir le monde du travail et de me former aux exigences de ce dernier.
+              </p>
+              <p class="text-white pt-3">< ></p>
+              <p class="pt-3">
+                Remplacement gérant restaurant ouvrier, veilleur de nuit hôtellerie, barman, ostréiculture, horticulture.
+              </p>
+            </div>
+          </div>
       </div>
       </div>
     </div>
