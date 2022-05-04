7b04639c1d42ac93f729462a40e18212122adf46
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-05-04 11:06:57 +0200

    experience container OK , Epitech OK

---
+++ fabien-portfolio/components/Experience.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--   SECTION Experience-->
+
+  <section class="pt-10 bg-gray-700" id="experience">
+    <div class="mx-auto my-container ">
+      <h1 class="text-4xl font-semibold tracking-widest text-white uppercase" >Expériences</h1>
+      <div class="mt-6 border-3 border-indigo-500 rounded shadow-xl">
+        <div class="px-12 py-16 bg-gray-800 rounded-xl">
+              <span class="block text-2xl font-semibold text-white uppercase">
+                  Projet Web EPITECH
+              </span>
+          <span class="block text-lg text-white uppercase">Novembre 2021 - En cours</span>
+          <div class="pt-4 text-lg font-semibold text-indigo-300 sm:text-xl">
+            <p>
+              Plus en détail, je suis formé sur des projets pratiques de façon hebdomadaire, avec un temps imparti à la réalisation de chaque projet et la possibilité de travailler sur plusieurs projets en parallèle, l’objectif étant d’être en capacité d’être polyvalent et flexible.
+            </p>
+            <p class="pt-3">
+              Vous pouvez retrouver l'ensemble des projets plus bas ou directement sur mon GitHub.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Experience"
+}
+</script>
+
+<style scoped>
+
+</style>
