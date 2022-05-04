e4f052168e2ce971f02a3a009e7dbad5620e8a3e
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-05-04 11:41:40 +0200

    Experience fresh burritos

--- fabien-portfolio/components/Experience.vue
+++ fabien-portfolio/components/Experience.vue
@@ -2,10 +2,11 @@
   <!--   SECTION Experience-->
 
   <section class="pt-10 bg-gray-700" id="experience">
-    <div class="mx-auto my-container ">
+    <div class="mx-auto my-container text-center ">
       <h1 class="text-4xl font-semibold tracking-widest text-white uppercase" >Expériences</h1>
       <div class="mt-6 border-3 border-indigo-500 rounded shadow-xl">
-        <div class="px-12 py-16 bg-gray-800 rounded-xl">
+        <div class="px-12 py-12 bg-gray-800 rounded-xl">
+          <div>
               <span class="block text-2xl font-semibold text-white uppercase">
                   Projet Web EPITECH
               </span>
@@ -14,11 +15,31 @@
             <p>
               Plus en détail, je suis formé sur des projets pratiques de façon hebdomadaire, avec un temps imparti à la réalisation de chaque projet et la possibilité de travailler sur plusieurs projets en parallèle, l’objectif étant d’être en capacité d’être polyvalent et flexible.
             </p>
+            <p class="text-white pt-3">< ></p>
             <p class="pt-3">
               Vous pouvez retrouver l'ensemble des projets plus bas ou directement sur mon GitHub.
             </p>
           </div>
         </div>
+
+        <div class="w-full my-10 border-b border-indigo-300 opacity-25"></div>
+        <div>
+              <span
+                class="block text-2xl font-semibold text-white uppercase"
+              >Chef d'équipe niveau v</span>
+          <span class="block text-lg text-white uppercase">FRESH BURRITOS RENNES</span>
+          <span class="block text-lg text-white uppercase">2016 - 2019 3 ans et 8 mois</span>
+          <div class="pt-4 text-lg text-indigo-300 font-semibold sm:text-xl">
+            <p>
+              Mon expériences ma permis d’acquérir en autonomie, en rigueur, tout en sachant être à l’écoute des besoins et demandes de mes manageurs et co-équipiers et des clients.
+            </p>
+            <p class="text-white pt-3">< ></p>
+            <p class="pt-3">
+              Gestion globale de l’exploitation, établir le planning des employés, recruter du personnel et le former aux procédures de l'établissement  ,maintien d’une qualité d’activité constante et productive.
+            </p>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   </section>
